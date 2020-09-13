5310c9753ac0ed53fdf8396282b03f583d835015
Rodman9110 <jregueira@oesia.com>
2020-09-13 23:20:15 +0200

    actualizar

---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+
+
+  devise_for :users
+  # get 'welcome/index'
+  resources :articles
+
+    # get "/articles"
+    # post "/articles"
+    # delete "/articles"
+    # get "/articles/:id"
+    # get "/articles/new"
+    # get "/articles/:id/edit"
+    # patch "/articles/:id"
+    # put "/articles/:id"
+
+
+  
+
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # HTTP
+  #   VERBOS:
+  #     GET
+  #     POST
+  #     PATCH
+  #     PUT
+  #     DELETE
+root "welcome#index"
+
+end
